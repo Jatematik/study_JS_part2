7d20bdc3dba7eb85c9f6d7188882d479884ed8d9
Jatematik <freerun_dimon@mail.ru>
2020-11-14 18:15:07 +0600

    feat: added setInterval

---
+++ script/script.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    // Timer
+    function countTimer(deadLine) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining(){
+            let dateStop = new Date(deadLine).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining/60)%60),
+            hours = Math.floor(timeRemaining/60/60);
+            return {timeRemaining, hours, minutes, seconds};
+        }
+
+        function updateClock(){
+            let timer = getTimeRemaining();
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+            timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+
+            if (timer.hours.toString().length < 2) {
+                timerHours.textContent = ('0' + timer.hours).slice(-2);
+            }
+
+            if (timer.timeRemaining < 1){
+                clearInterval(setInterval(updateClock, 1000));
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }
+        
+        setInterval(updateClock, 1000);
+        updateClock();
+    }
+
+    countTimer('20 nov 2020 00:00');
+
+});
